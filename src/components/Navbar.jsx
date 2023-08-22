b350a5eb3771a1c62d152173e0275a2ff9ff9ab4
Kiprich <davidkipkemoi30@gmail.com>
2023-08-22 22:43:53 +0300

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,14 +27,42 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt='logo' className='w-20 h-25 object-contain' />
+          <img src={logo} alt='logo' className='w-20 h-20 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             David &nbsp;
             <span className='sm:block hidden'> | Kipkemoi</span>
           </p>
         </Link>
-
-
+          <ul className='list-none hidden sm:flex flex-row gap-10'>
+              {
+                navLinks.map((link)=>(
+                  <li
+                  key = {link.id}
+                  className={`${
+                    active === link.title ? "text-white" :
+                    "text-secondary"
+                  } hover:text-white text-[20px] font-medium cursor-pointer`}
+                  onClick={()=>{
+                    setActive(link.title)
+                  }}
+                  
+                  >
+                    <a href={`#${link.id}`}>
+                      {link.title}
+                    </a>
+                  </li>
+                ))
+              }
+          </ul>
+            <div className='md:hidden flex flex-1 justify-end items-center'>
+              <img 
+              src={toggle ? close : menu}
+              alt='menu'
+              className='w-[28px] h-[25px]
+              object-contain cursor-pointer'
+              onClick={()=>setToggle(!toggle)}
+              />
+            </div>
       </div>
     </nav>
   ) 
