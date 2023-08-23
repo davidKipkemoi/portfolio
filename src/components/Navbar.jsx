aa2a7f2571db0e7ffe3d7cfdf3b1609922d3241c
Kiprich <davidkipkemoi30@gmail.com>
2023-08-23 09:08:25 +0300

    threejs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -54,7 +54,7 @@ const Navbar = () => {
                 ))
               }
           </ul>
-            <div className='md:hidden flex flex-1 justify-end items-center'>
+            <div className='sm:hidden flex flex-1 justify-end items-center'>
               <img 
               src={toggle ? close : menu}
               alt='menu'
@@ -62,6 +62,33 @@ const Navbar = () => {
               object-contain cursor-pointer'
               onClick={()=>setToggle(!toggle)}
               />
+              <div
+              className={`${!toggle ? 'hidden' :
+              'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+
+                <ul className='list-none flex justify-end items-start flex-col gap-5'>
+              {
+                navLinks.map((link)=>(
+                  <li
+                  key = {link.id}
+                  className={`${
+                    active === link.title ? "text-white" :
+                    "text-secondary"
+                  } font-poppins font-medium cursor-pointer text-[16px]`}
+                  onClick={()=>{
+                    setToggle(!toggle)
+                    setActive(link.title)
+                  }}
+                  
+                  >
+                    <a href={`#${link.id}`}>
+                      {link.title}
+                    </a>
+                  </li>
+                ))
+              }
+          </ul>
+              </div>
             </div>
       </div>
     </nav>
