743eee72eb02edfbe89e187441cdc0a7d9d98ded
Kiprich <davidkipkemoi30@gmail.com>
2023-09-25 14:14:23 +0300

    contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import React,{useState,useEffect,useRef} from 'react'
+import {motion} from 'framer-motion'
+import {EarthCanvas} from './canvas'
+import {SectionWrapper} from '../hoc'
+import {slideIn} from '../utils/motion'
 
+import {styles} from '../styles'
+import Emailjs from '@emailjs/browser'
 const Contact = () => {
+  const formRef = useRef()
+  const [form,setForm] = useState({
+    name:'',
+    email:'',
+    message:'',
+  })
+  const [loading,setLoading]=useState(false)
+  const handleChange = (e)=>{
+    const {target} = e
+    const {name,value} = target
+    setForm({
+      ...form,
+      [name]:value,
+    })
+  }
+
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+    setLoading(true)
+    Emailjs.send()
+  }
   return (
-    <div>Contact</div>
+    <div
+    className='xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden'
+    >
+      <motion.div
+      variants={slideIn("left","tween",0.2,1)}
+      className='flex=[0.75] bg-black-100 p-8 rounded-2xl w-3/4'
+      >
+          <p className={styles.sectionSubText}>Contact US</p>
+          <h3 className={styles.sectionHeadText}>Contact</h3>
+
+           <form 
+          onSubmit={handleSubmit}
+           className='mt-12 flex flex-col gap-8'
+           ref={formRef}>
+
+            <label className='flex flex-col'>
+              <span className='text-white font-medium mb-4'>Your Name is Mumo</span>
+              <input 
+              type="text"
+              name="name"
+              value={form.name}
+              placeholder='What is Your Name ?'
+              className='bg-white py-4 px-6 placeholder:text-secondary text-black rounded-2xl outline-none border-none font-medium'
+              onChange={handleChange}              />
+              
+            </label>
+            <label className='flex flex-col'>
+              <span className='text-white font-medium mb-4'>Your Email</span>
+              <input 
+              type="text"
+              name="email"
+              value={form.email}
+              placeholder='What is Your Email ?'
+              className='bg-white py-4 px-6 placeholder:text-secondary text-black rounded-2xl outline-none border-none font-medium'
+              onChange={handleChange}              />
+              
+            </label>
+            <label className='flex flex-col'>
+              <span className='text-white font-medium mb-4'>Message</span>
+              <textarea 
+              type="text"
+              name="message"
+              rows={5}
+              value={form.message}
+              placeholder='Enter your Message'
+              className='bg-white py-4 px-6 placeholder:text-secondary text-black rounded-2xl outline-none border-none font-medium'
+              onChange={handleChange}              />
+              
+            </label>
+            <button
+            type='submit'
+            className='bg-tertiary py-4 px-8 rounded-xl w-full outline-none text-white font-bold shadow-md shadow-primary'
+            >
+              {loading ?"Sending..":"Send"}
+            </button>
+            </form> 
+      </motion.div>
+    </div>
   )
 }
 
-export default Contact
+export default SectionWrapper(Contact,"contact")
--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import {motion} from 'framer-motion'
+import {styles} from '../styles'
+import {SectionWrapper} from '../hoc'
+import {testimonials} from '../constants'
+import {fadeIn,textVariant} from '../utils/motion'
 
+const FeedBackCard = ({index,testimonial,name,designation,company,image})=>(
+  <motion.div
+  variant={fadeIn("","spring",index*0.5,0.75)}
+  className='bg-black-200 p-9 rounded-3xl sm:w-[320px] w-full'
+  >
+    <p className='text-white font-black text-[48]px'> "</p>
+    <p>
+      {testimonial}
+
+    </p>
+  </motion.div>
+)
 const Feedbacks = () => {
   return (
-    <div>Feedbacks</div>
+    <div className='mt-2 bg-black rounded-[20px] '>
+      <div className={`${styles.padding} 
+       bg-tertiary rounded-2xl min-h-[300px]`}>
+        <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>
+              What Others Say
+          </p>
+          <h2 className={styles.sectionHeadText}>Testimonials</h2>
+        </motion.div>
+      </div>
+      <div className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-7`}>
+         {testimonials.map((testimonial,index)=>(
+            <FeedBackCard 
+            key={testimonial.name}
+            index = {index}
+            {...testimonial}
+            />
+         ))
+
+         }
+      </div>
+    </div>
   )
 }
 
